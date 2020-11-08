1646793cd89474efd4d4438535cd48d74da4b197
zhangning <648296598@qq.com>
2020-11-08 21:01:05 +0800

    2

--- duyi-vue/src/App.vue
+++ duyi-vue/src/App.vue
@@ -1,29 +1,30 @@
-<template>
-  <div id="app">
-   <base-slots v-slot='slotProps'>{{ slotProps.text }}</base-slots>
-  </div>
-</template>
+
 
 <script>
-import BaseData from './components/BaseData';
-import BaseSlots from './components/BaseSlots';
-
 export default {
-  name: 'App',
-  components: {
-    BaseData,
-    BaseSlots,
+  name:'app',
+  data(){
+    return {
+      value:'hello world',
+      show:false
+    }
+  },
+  components:{
+
+  },
+  render(h){
+    return (<h1>{ this.value }
+      <div domPropsTextContent='<a>lianjie</a>'></div>
+      <div v-show={this.show}>show</div>
+      {false && <div>v-if</div>}
+      
+    </h1>
+            
+    )
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
